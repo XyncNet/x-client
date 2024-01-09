aee4266492ff8cdf1dd4dd2e493150dde1d50feb
Mike <mixartemev@gmail.com>
2024-01-09 11:50:44 +0300

    repeat decorator

--- http_client/__init__.py
+++ http_client/__init__.py
@@ -1,1 +1,18 @@
+from aiohttp import ClientOSError
+
 from http_client.client import Client
+
+#decorator for network funcs
+def repeat(times: int = 5, wait: int = 3):
+    def decorator(func: callable):
+        from asyncio import sleep
+        async def wrapper(*args, **kwargs):
+            for attempt in range(times):
+                try:
+                    return await func(*args, **kwargs)
+                except ClientOSError as e:
+                    print(f'{func.__name__}: attempt {attempt+1}:',  e)
+                    await sleep(wait)
+            return print('Patience over!')
+        return wrapper
+    return decorator

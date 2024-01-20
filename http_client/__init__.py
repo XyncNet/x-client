dad7f4a54d795e10f4e169a03f47525ffbd91a6b
Mike <mixartemev@gmail.com>
2024-01-20 21:44:10 +0300

    catch all Exception

--- http_client/__init__.py
+++ http_client/__init__.py
@@ -1,17 +1,14 @@
-from aiohttp import ClientOSError
-from aiohttp.http_exceptions import HttpProcessingError
-
 from http_client.client import Client
 
 #decorator for network funcs
-def repeat(times: int = 5, wait: int = 3):
+def repeat(times: int = 4, wait: int = 3):
     def decorator(func: callable):
         from asyncio import sleep
         async def wrapper(*args, **kwargs):
             for attempt in range(times):
                 try:
                     return await func(*args, **kwargs)
-                except (ClientOSError, HttpProcessingError) as e:
+                except Exception as e:
                     print(f'{func.__name__}: attempt {attempt+1}:',  e)
                     await sleep(wait)
             return print('Patience over!')

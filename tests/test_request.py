29504fc590bb812bc875772cea5949e0d1c08907
Mike <mixartemev@gmail.com>
2024-10-31 16:23:25 +0300

    fix: refact

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,7 +1,7 @@
-from http_client import Client
+from x_client.aiohttp import Client
 
 
 async def test_public_request():
     pub = Client()
-    resp = await pub.get('https://dapp.deals')
-    assert resp.startswith('<!DOCTYPE html>'), "Bad request"
+    resp = await pub._get("https://xync.net")
+    assert resp.startswith("<!DOCTYPE html>"), "Bad request"

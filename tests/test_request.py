13336f470a0106ad8c13d62c685d571a3f911f89
Mike <mixartemev@gmail.com>
2024-12-13 17:15:32 +0700

    fix:brotli

--- tests/test_request.py
+++ tests/test_request.py
@@ -2,6 +2,6 @@ from x_client.aiohttp import Client
 
 
 async def test_public_request():
-    pub = Client()
-    resp = await pub._get("https://xync.net")
+    pub = Client("xync.net")
+    resp = await pub._get("")
     assert resp.startswith("<!DOCTYPE html>"), "Bad request"

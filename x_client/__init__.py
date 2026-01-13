5682b33532ee1a30da47a10ac71efecc29d3b480
xync <mixartemev@gmail.com>
2026-01-13 16:00:46 +0300

    fix: user-agent upd

--- x_client/__init__.py
+++ x_client/__init__.py
@@ -3,7 +3,7 @@ from aiohttp.http_exceptions import HttpProcessingError
 df_hdrs = {
     "accept": "application/json",
     "cookie": ";",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
 }
 
 
